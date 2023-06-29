b9cfbe744032bb3e88bcb9665682a85457a0972f
lgregi <lucas.silva62899@gmail.com>
2023-06-29 09:10:58 -0300

    first commit

--- src/app/painel/frase-mock.ts
+++ src/app/painel/frase-mock.ts
@@ -2,8 +2,8 @@ import { Frase } from "../Sharerd/frase.model";
 // aqui foi recebida a classe criada na pasta share e atribuida os valores 
 export const FRASES: Frase[]= [
     {FraseEng : "I LIKE TO LEARN", FrasePTbr:'Eu gosto de aprender'},
-    {FraseEng : "I WATCH TV ", FrasePTbr:'Eu assisto tv'},
-    {FraseEng : "HOW ARE YOU?", FrasePTbr:'Como vai você?'},
+    {FraseEng : "I WATCH TV ", FrasePTbr:'Eu assisto TV'},
+    {FraseEng : "HOW ARE YOU?", FrasePTbr:'Como você está?'},
     {FraseEng : "I EAT BREAD", FrasePTbr:'Eu como pão'}
     // esta const joga todos os atributos da classe Frase dentro de um vetor e permite atribuir valores a eles
 ]
