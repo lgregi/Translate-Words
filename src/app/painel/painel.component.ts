a2c3ac0fd9eaa9a9644d2f3dac58e5ac9f86b338
Lucas André <106933976+lgregi@users.noreply.github.com>
2023-08-14 21:32:17 -0300

    Update painel.component.ts

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, OnDestroy} from '@angular/core';
+import { Component, EventEmitter, Output, OnDestroy } from '@angular/core';
 import { Frase } from '../Sharerd/frase.model';
 import { FRASES } from './frase-mock';
 
@@ -9,80 +9,74 @@ import { FRASES } from './frase-mock';
 })
 export class PainelComponent implements OnDestroy {
 
-  public frases:Frase[] =FRASES // atributo que recebe as frase da mock
-  public instrucao: string= 'TRADUZA ESSA FRASE'
-  public resposta:string= ''
-  public faz:string= ''
+  public frases: Frase[] = FRASES // atributo que recebe as frase da mock
+  public instrucao: string = 'TRADUZA ESSA FRASE'
+  public resposta: string = ''
+  public upper: string = ''
+  public faz: string = ''
 
-  public rodada:number= 0
-  public rodadaFrase:Frase
-  public progresso: number= 0 // criada para atribuir valor a variavel de outro component
+  public rodada: number = 0
+  public rodadaFrase: Frase
+  public progresso: number = 0 // criada para atribuir valor a variavel de outro component
 
 
-  public tentativas:number= 3
+  public tentativas: number = 3
   //serve para se comunicar com a classe pai
-  @Output() public encerrarJogo: EventEmitter<string>= new EventEmitter()
-  
-  constructor(){
+  @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter()
+
+  constructor() {
     this.Atualizadodada()
     this.rodadaFrase = this.frases[this.rodada];//// recebe a frase da posica de rodada
     //console.log(this.rodadaFrase)
-    
+
   }
   ngOnDestroy() {
     //console.log('componente painel foi destruido')
-      
+
   }
-  
-  atualizaresposta(resposta: Event):void // precisa colocar o event para receber o evento
-{
-  // função que esta sendo utilizada toda vez que há imput de dados 
-  // <textarea class="form-control" rows="3" (input)="atualizaresposta($event)"></textarea> no angula para recupera os dados diguitados
-  this.resposta=((<HTMLInputElement>resposta.target).value)
-  //console.log(this.resposta)
 
-}
-testandominhalabel(respos:Event):void{
-  this.faz =((<HTMLInputElement>respos.target).value)
-  console.log(this.faz)
+  atualizaresposta(resposta: Event): void // precisa colocar o event para receber o evento
+  {
+    
+    this.resposta = ((<HTMLInputElement>resposta.target).value)
+    this.upper = this.resposta.toUpperCase()
+    //console.log(this.resposta)
 
-}
-
-
-verificaresposta():void {
-  //console.log( 'verificaresposta', this.resposta)
-
-  if(this.rodadaFrase.FrasePTbr == this.resposta){
-    //alert(' frase está correta')
-    this.rodada++
-
-    //this.progresso= this.progresso +25//ou
-    this.progresso= this.progresso +(100/this.frases.length)
-   // console.log(this.progresso)
-    if(this.rodada===4){
-      this.encerrarJogo.emit('vitoria')
-      alert('Traduções concluídas com sucesso!')
-    }
-    //atualiza a rodada
-    this.Atualizadodada()
-    //this.rodadaFrase = this.frases[this.rodada] // recebe a frase da posica de rodada
-    //console.log(this.rodadaFrase)
-
-   
-
-  } else{
-    alert('a tradução está errada')
-    this.tentativas --
-    if(this.tentativas==-1){
-      this.encerrarJogo.emit('derrota')
-      alert('você perdeu todas as tentativas')
-    }
   }
- // console.log(this.tentativas)
-}
- Atualizadodada() : void{
-  this.rodadaFrase = this.frases[this.rodada];//// recebe a frase da posica de rodada
-  this.resposta='' // limpa a resposta
- }
+  testandominhalabel(respos: Event): void {
+    this.faz = ((<HTMLInputElement>respos.target).value)
+    console.log(this.faz)
 
+  }
+
+
+  verificaresposta(): void {
+
+    if (this.rodadaFrase.FrasePTbr == this.upper) {
+
+      this.rodada++
+      this.progresso = this.progresso + (100 / this.frases.length)
+      if (this.rodada === 4) {
+        this.encerrarJogo.emit('vitoria')
+        alert('Traduções concluídas com sucesso!')
+      }
+      this.Atualizadodada()
+
+    } else {
+      alert('a tradução está errada')
+      this.tentativas--
+      this.resposta = '' // limpa a resposta
+      if (this.tentativas == -1) {
+        this.encerrarJogo.emit('derrota')
+        alert('você perdeu todas as tentativas')
+      }
+    }
+    // console.log(this.tentativas)
+  }
+  Atualizadodada(): void {
+    this.rodadaFrase = this.frases[this.rodada];
+    this.resposta = ''
+    this.upper = ''
+
+  }
 }
